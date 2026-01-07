27e185464867f07d2a9cbacd551105cb967ad148
Mohamed-elsayed1 <mohamed.elsayed@psinv.net>
2026-01-07 12:04:05 +0200

    Setup Default colors

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,15 @@
+import { Button } from '@/components/ui/button';
 import { useTranslations } from 'next-intl';
 
 export default function HomePage() {
     const t = useTranslations('HomePage');
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <div className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <h1 className="text-3xl font-semibold">{t('title')}</h1>
+        <div className="flex min-h-screen items-center justify-center font-sans">
+            <div className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+                <h1 className="text-2xl">{t('title')}</h1>
+                <Button variant={"secondary"}>Button</Button>
+                <Button variant={"secondary"}>Button</Button>
+                <Button variant={"secondary"}>Button</Button>
             </div>
         </div>
     );
